60273da61503f2e5c362d560be2236784a6b5d69
Ioan Loosley <legit.ioangogo@gmail.com>
2016-02-03 12:36:37 +0000

    Fixed spam problem

--- getrss.py
+++ getrss.py
@@ -1,13 +1,22 @@
+import feedparser
+import time
+global lastcheck
+import html.parser
+html_parser = html.parser.HTMLParser()
+from time import sleep
+lastcheck = time.gmtime()
+print("Get rss Loaded")
 def fetchitems(url):
     msg = []
     feed = feedparser.parse(url)
-    for item in feed.entries:
-        if item.published_parsed > lastcheck:
-            msg.append("New Content By " + item.author + ": " + html_parser.unescape(item.title) + " " + html_parser.unescape(item.link))
-            continue
-        else:
-            break
-        break
+    try:
+        for item in feed.entries:
+            if item.published_parsed >= lastcheck:
+                msg.append("New Content By " + item.author + ": " + html_parser.unescape(item.title) + " " + html_parser.unescape(item.link))
+            else:
+                break
+    except Exception as e:
+        msg=[e]
     return msg
 
 def rssfunc(q, feeds):
@@ -16,3 +25,5 @@ def rssfunc(q, feeds):
             q.put(fetchitems(stuff))
         lastcheck = time.gmtime()
         sleep(60)
+        q.queue.clear()
+
