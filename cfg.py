287927caa11428abd7d6f3ea6ce41fb3ea52a9cd
Ioan Loosley <legit.ioangogo@gmail.com>
2016-02-03 16:08:40 +0000

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -5,4 +5,4 @@ IDENT = NICK
 REALNAME = NICK
 CHANNEL = "changeme"
 KEY = ""
-FEEDS = []
+FEEDS = ["https://www.youtube.com/feeds/videos.xml?user=eevblog","https://www.youtube.com/feeds/videos.xml?user=TheSignalPathBlog","https://www.youtube.com/feeds/videos.xml?user=mikeselectricstuff","https://www.youtube.com/feeds/videos.xml?user=eevblog2","https://marvintestfeed.wordpress.com/feed/atom/"]
