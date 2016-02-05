64c423a58a45fa268d137ae57470f3c5f5f4bcd0
Ioan Loosley <legit.ioangogo@gmail.com>
2016-02-05 00:06:58 +0000

    Im tired i need sleep

--- getrss.py
+++ getrss.py
@@ -1,19 +1,28 @@
 import feedparser
 import time
-import datetime
 global lastcheck
 import html.parser
 html_parser = html.parser.HTMLParser()
 from time import sleep
-lastcheck = datetime.datetime.now()
+lastcheck = time.gmtime()
 print("Get rss Loaded")
 def fetchitems(url):
     msg = []
-    feedcount=0
     feed = feedparser.parse(url)
-    print(lastcheck)
     try:
         for item in feed.entries:
-            if datetime.datetime.fromtimestamp(time.mktime(item.published_parsed)) >= lastcheck:
+            if item.published_parsed >= lastcheck:
                 msg.append("New Content By " + item.author + ": " + html_parser.unescape(item.title) + " " + html_parser.unescape(item.link))
-                feedcount += 1
+            else:
+                break
+    except Exception as e:
+        msg=[e]
+    return msg
+
+def rssfunc(q, feeds):
+    while True:
+        for stuff in feeds:
+            q.put(fetchitems(stuff))
+        lastcheck = time.gmtime()
+        sleep(60)
+        q.queue.clear()
