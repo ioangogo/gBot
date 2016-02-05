04a84aa2574a2964fe96dba2e6716bf3731bf1d0
Ioan Loosley <legit.ioangogo@gmail.com>
2016-02-05 00:37:54 +0000

    :P

--- getrss.py
+++ getrss.py
@@ -1,37 +1,48 @@
 import feedparser
 import time
+import datetime
 global lastcheck
 import html.parser
-import urllib.request
+html_parser = html.parser.HTMLParser()
+from time import sleep
+lastcheck = datetime.datetime.now()
+print("Get rss Loaded")
+import urllib
+
 
 headers = {
     'User-Agent': 'Ibot: Ioans bot',
 }
 
-html_parser = html.parser.HTMLParser()
-from time import sleep
-lastcheck = time.gmtime()
-print("Get rss Loaded")
 def downloadtxt(url):
-    getstuff = requests.get(url)
-    return getstuff.content
+    getstuff = urllib.request.openurl(url)
+    return getstuff
 def fetchitems(url):
     msg = []
-    feed = feedparser.parse(downloadtxt(url))
+    feedcount=0
+    feed = feedparser.parse(url)
+    print(lastcheck)
     try:
         for item in feed.entries:
-            if item.published_parsed > lastcheck:
+            if datetime.datetime.fromtimestamp(time.mktime(item.published_parsed)) >= lastcheck:
                 msg.append("New Content By " + item.author + ": " + html_parser.unescape(item.title) + " " + html_parser.unescape(item.link))
+                print("Published: " + datetime.datetime.fromtimestamp(time.mktime(item.published_parsed)) + " Last Checked: " + lastcheck)
+                feedcount += 1
             else:
                 break
     except Exception as e:
-        msg=[e]
+        print(e)
     return msg
+
 
 def rssfunc(q, feeds):
     while True:
+        returnmsg=[]
+        print("Feed Fetch Start")
         for stuff in feeds:
-            q.put(fetchitems(stuff))
-        lastcheck = time.gmtime()
-        sleep(60)
-        q.queue.clear()
+            returnmsg += fetchitems(stuff)
+#        print(returnmsg)
+        q.put(returnmsg)
+        print("Feed Fetch Done")
+        lastcheck = datetime.datetime.now()
+        sleep(120)
